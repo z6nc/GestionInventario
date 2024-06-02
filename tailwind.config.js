bfff2ed7c646ac5298424b9abe2bc789b2a63cd4
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-02 15:34:08 -0500

    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Principal: ['Gagalin', 'sans-serif'], // Cambiar a Gagalin
+        Secundario: ['Belleza', 'sans-serif'], // Cambiar a Gagalin
+      },
+      colors: {
+        primario: '#737373',
+        ColorInput: '#CDC7BC',
+      },
+      backgroundColor:{
+        butonPrincipal: '#150D0A',
+      },
+    },
+  },
+  plugins: [],
+}

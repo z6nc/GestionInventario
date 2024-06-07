bc4b3397ccea346deb1669d7a71013534e0224f7
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-07 13:45:56 -0500

    LEER IMPORTANTE

--- src/componets/navbar.php
+++ src/componets/navbar.php
@@ -2,6 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/Font.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Barlow:wght@500&display=swap">
+  <link rel="stylesheet" href="css/EstilosPrincipal.css">
+   
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>

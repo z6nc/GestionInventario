426c1321e7a17791c88eb293f615d73b46e340bf
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-07 11:04:14 -0500

    chore: Update login form styling and optimize code

--- src/Inicio.php
+++ src/Inicio.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./output.css" rel="stylesheet">
+
     <title>Document</title>
 </head>
 <body>
-    <h1>hola mudnos</h1>
+<h1>sdkkas</h1>
+ 
 </body>
 </html>

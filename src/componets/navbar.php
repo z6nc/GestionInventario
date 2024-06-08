bc215824d71bd570e7c8e23ba835530655e8fc20
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-08 12:39:44 -0500

    chore: Update navbar styling, add logout button, and optimize code

--- src/componets/navbar.php
+++ src/componets/navbar.php
@@ -55,6 +55,10 @@
           </li>
 
         </ul>
+        <div class="botonCerrar">
+        <i class="fas fa-door-open"></i>
+          <a href="">Cerrar Session</a>
+        </div>
     </div>
 </body>
 <style>
@@ -78,6 +82,9 @@
     background-color: #2A3542;
     list-style: none;
     display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
     height: 100%;
    
   }
@@ -91,18 +98,34 @@
     display: flex;
     align-items: center;
     gap: 7px;
+   transition: all .1s ease-in-out;
+
   }
   .navbar .container-Second ul li  i {
    color: white;
   }
   .navbar .container-Second ul li:hover {
-    background-color: #a9a9a9 ;
-    border-radius: 5px ;
+   transform: scale(1.05);
 }
   .navbar .container-Second ul li a{
    color: white;
    font-size: 17px;
     text-decoration: none;
+  }
+  .navbar .container-Second .botonCerrar{
+    background-color: #16A085;
+    width: 100%;
+    padding: 20px 0px;
+  }
+  .navbar .container-Second .botonCerrar a{
+  color: white;
+  text-decoration: none;
+  font-size: 17px;
+  }
+  .navbar .container-Second .botonCerrar i {
+   color: white;
+  padding-left: 14px;
+
   }
 </style>
 </html>

47bbf4606ae5573e8cc8df2af39f09309e2d58a2
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-11 11:52:11 -0500

    chore: Update informacion.php layout and product information

--- src/componets/informacion.php
+++ src/componets/informacion.php
@@ -3,47 +3,48 @@
 
     <aside class="main-Container-grid">
       <div class="container-information">
-      <i class="fas fa-box product-icon"></i>
-          <div>
-            <p class="count">4</p>
-          <a href="">Registrar producto</a>
-          </div>
+        <div>
+        <a href="">Ganancias por Venta</a>
+          <p class="count">4</p>
+        </div>
+        <i class="fas fa-coins"></i>
       </div>
 
-      <div class="ontainer-information-Two">
-      <i class="fas fa-clipboard-check registered-product-icon"></i>
-          <div>
-            <p class="count">3</p>
-          <a href="">Producto registrados</a>
-          </div>
+      <div class="container-information-Two">
+        <div>
+        <a href="">Producto registrados</a>
+          <p class="count">3</p>
+        </div>
+        <i class="fas fa-clipboard-check registered-product-icon"></i>
       </div>
 
-      <div class="ontainer-information-Three">
-          <img src="" alt="">
-          <div>
-            <p class="Count">4</p>
-            <a href="">Producto por Vencer </a>
-          </div>
+      <div class="container-information-Three">
+        <div>
+        <a href="">Producto por Vencer </a>
+          <p class="Count">4</p>
+        </div>
+        <i class="fas fa-calendar-alt"></i>
+
       </div>
 
     </aside>
-     
+
     <aside class="main-Container-table">
-       <div class="Container-table-Product">
+      <div class="Container-table-Product">
         <h2 class="font-Principal">Producto Recientemente Añadidos</h2>
-          <div>
+        <div>
           <h4>SACO DE ARROZ DE MAR</h4>
           <p> CONDIMENTO</p>
-          </div>
-       </div>
+        </div>
+      </div>
 
-       <div class="Container-table-Stock">
+      <div class="Container-table-Stock">
         <h2 class="font-Principal">Producto con Stock mínimo</h2>
-          <div>
+        <div>
           <h4>Aceite Vegetal PRIMOR 1.8L</h4>
           <p> Abarrotes</p>
-          </div>
-       </div>
+        </div>
+      </div>
     </aside>
   </article>
 </section>
@@ -51,23 +52,43 @@
   .Informacion {
     background-color: #F1F2F7;
     grid-area: c;
-    
+
   }
+
   .main-Container {
     display: grid;
     grid-template-rows: 1fr 1fr;
     gap: 20px;
-    padding: 20px;
-    margin: 0px 10px;
+    padding: 16px;
   }
+
   .main-Container .main-Container-grid {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     gap: 20px;
   }
-  .main-Container .main-Container-table{
-    display: grid ;
-    grid-template-columns: 1fr 1fr;
+
+  .main-Container .main-Container-grid .container-information {
+    display: flex;
+    background: white;
+    justify-content: space-between;
+    padding: 0 24px;
+    align-items: center;
+    border-radius: 9px;
+    border-left: 9px solid #FFC107;
   }
 
+  .main-Container .main-Container-grid .container-information i {
+    font-size: 50px;
+  }
+
+ 
+
+
+
+
+  .main-Container .main-Container-table {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
 </style>

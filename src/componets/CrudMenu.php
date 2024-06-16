2ba823f69a1c186b58153806dfcee4db01fc7d79
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-16 16:45:35 -0500

    refactor: Improve menu layout and add "Agregar Menu" button

--- src/componets/CrudMenu.php
+++ src/componets/CrudMenu.php
@@ -1,19 +1,18 @@
 
 
 
-
 <section style="background-color: #f1f1f1; " class="Container-Menu">
-    <article style="width: 100%;display: flex; justify-content:space-between; align-items: center; padding: 2px 10px; background-color: white; max-width: 90%; margin: auto; margin-top: 3%; ">
-   <div style="display: flex;  justify-content: center ; align-items: center; gap: 10px;">
-   <i class="fas fa-utensils"></i>
-   <h1 class="font-Principal"> MENU</h1>  
+  
+    <article class="shadows" style="width: 100%;display: flex; justify-content:space-between; align-items: center; padding: 10px 10px; background-color: white; max-width: 90%; margin: auto; margin-top: 3%; border-radius: 5px; ">
+   <div style="display: flex;  justify-content: center ; align-items: center; gap: 10px; ">
+   <i style="font-size: 22px;   "  class="fas fa-utensils"></i>
+   <h1 class="font-Principal" style="font-size: 22px; letter-spacing: 2px;"> MENU</h1>  
    </div>
-    
-        <a href="CrudMenu.php" class="btn btn-primary">Agregar Menu</a>
+        <a href="#" style="background-color:#198754 ; padding: 7px 9px; text-decoration: none; border-radius: 5px; color: white;" >Agregar Menu</a>
     </article>
     
-<table class="" style=" width: 100%; background-color: white; max-width: 90%; margin: auto; margin-top: 20px;">
-<thead class=" " style="border: 2px solid black;"  >
+<table class="shadows" style=" width: 100%; background-color: white; max-width: 90%; margin: auto; margin-top: 20px; ">
+<thead class=" " style=" background-color: red; "  >
     <tr class=""  >
       <th scope="col">#</th>
       <th scope="col">IDMENU</th>
@@ -40,5 +39,7 @@
 </section>
 
 <style>
-   
+.shadows{
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+}
 </style>

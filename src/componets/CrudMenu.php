0cdf840ccdf15c3e34e808909da4fb4425dbbc36
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-16 13:58:31 -0500

    chore: Update CrudMenu.php to add table structure and styling

--- src/componets/CrudMenu.php
+++ src/componets/CrudMenu.php
@@ -1,11 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+
+
+
+<section style="background-color: #f1f1f1; " class="Container-Menu">
+<table class="" style=" width: 100%; background-color: white; max-width: 90%; margin: auto; margin-top: 20px;">
+  <thead class=" " style="border: 2px solid black;"  >
+    <tr class=""  >
+      <th scope="col">#</th>
+      <th scope="col">IDMENU</th>
+      <th scope="col">NOMBRE MENU</th>
+      <th scope="col">PRECIO MENU </th>
+      <th scope="col">ACCIONES </th>
+
+    </tr>
+  </thead>
+  <tbody class="table-group-divider " style="text-align: center;">
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>
+        <button type="button" class="btn btn-primary">Editar</button>
+        <button type="button" class="btn btn-danger">Eliminar</button>
+      </td>
+
+    </tr>
+  </tbody>
+</table>
+</section>
+
+<style>
+   
+</style>

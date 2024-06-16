00aab77b89fdd5d6b2b5b319327e89dbd6dc8500
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-16 16:18:41 -0500

    Update CrudMenu.php to improve menu layout and add "Agregar Menu" button

--- src/componets/CrudMenu.php
+++ src/componets/CrudMenu.php
@@ -3,8 +3,17 @@
 
 
 <section style="background-color: #f1f1f1; " class="Container-Menu">
+    <article style="width: 100%;display: flex; justify-content:space-between; align-items: center; padding: 2px 10px; background-color: white; max-width: 90%; margin: auto; margin-top: 3%; ">
+   <div style="display: flex;  justify-content: center ; align-items: center; gap: 10px;">
+   <i class="fas fa-utensils"></i>
+   <h1 class="font-Principal"> MENU</h1>  
+   </div>
+    
+        <a href="CrudMenu.php" class="btn btn-primary">Agregar Menu</a>
+    </article>
+    
 <table class="" style=" width: 100%; background-color: white; max-width: 90%; margin: auto; margin-top: 20px;">
-  <thead class=" " style="border: 2px solid black;"  >
+<thead class=" " style="border: 2px solid black;"  >
     <tr class=""  >
       <th scope="col">#</th>
       <th scope="col">IDMENU</th>

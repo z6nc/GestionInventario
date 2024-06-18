ed3bef414819cafefe1513b092c27a4587ab4308
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-18 16:42:22 -0500

    Refactor menu layout and improve form styling in AgregarMenu.php

--- src/componets/AgregarMenu.php
+++ src/componets/AgregarMenu.php
@@ -1,8 +1,15 @@
 <section style="background-color: whitesmoke; ">
- <form style="background-color: white;" >
-  <div class="">
-    <label for="exampleInputEmail1" class="form-label">Nombre del Menu</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+<div style="display: flex;align-items: center;  margin: .3% 3%; gap:5px;" class="Container_House">
+      <a href="" > <i  style="color: #16a085;" class="fas fa-home"></i></a>
+      <p style="color:#5c5b60;">/ Menu</p>
+     </div>
+ <form class="shadows" style="background-color: white; max-width: 60%; margin: auto; display: flex ; flex-direction: column; margin-top: 3%; gap: 12px;" > 
+ <div style="background-color: #16a085;">
+        <h1  style=" margin-top: 20px;color: white; padding-left: 20px; ">REGISTRAR NUEVO USUARIO</h1>
+</div>
+ <div class="" style="padding: 0px 22px; display:flex; gap: 10px; align-items: center;">
+    <label style="font-size: 21px;" for="exampleInputEmail1" class="form-label">Nombre del Menu:</label>
+    <input style="padding: 6px 3px; font-size: 17px;" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
   </div>
   <div class="">
     <label for="exampleInputPassword1" class="form-label">Precio Menu</label>
@@ -12,7 +19,17 @@
     <label for="exampleInputPassword1" class="form-label">Estado del Menu</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <div>
+  <button type="submit" class="btn btn-primary">Guardar</button>
+  <button type="submit" class="btn btn-primary">Volver</button>
+  </div>
+ 
+
 </form>
 
 </section>
+<style>
+    .shadows{
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+}
+</style>

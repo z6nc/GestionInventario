fb1d5c56f296b2b2af12ab0bd7af9612f5f505ec
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-18 11:50:21 -0500

    Refactor menu layout and add "Agregar Menu" button

--- src/componets/CrudMenu.php
+++ src/componets/CrudMenu.php
@@ -1,13 +1,39 @@
+
+
 <section style="background-color: #f1f1f1; " class="Container-Menu">
   
-    <article class="shadows" style="width: 100%;display: flex; justify-content:space-between; align-items: center; padding: 10px 10px; background-color: white; max-width: 90%; margin: auto; margin-top: 3%; border-radius: 5px; ">
-   <div style="display: flex;  justify-content: center ; align-items: center; gap: 10px; ">
-   <i style="font-size: 22px;   "  class="fas fa-utensils"></i>
-   <h1 class="" style="font-size: 22px; letter-spacing: 2px;"> MENU</h1>  
-   </div>
-        <a href="#" style="background-color:#198754 ; padding: 7px 9px; text-decoration: none; border-radius: 5px; color: white;" >Agregar Menu</a>
+    <article style=" padding:1.5% 5% 0% 5%;">
+     <div style="display: flex;align-items: center;  margin: 12px 0px;" class="Container_House">
+      <a href="" > <i  style="color: #16a085;" class="fas fa-home"></i></a>
+      <p >/ Menu</p>
+     </div>
+
+     <div style="display: flex; align-items: center; justify-content: space-between; " class="Container_Filtro">
+      <div class="Container_files">
+        <button><a href=""><i class="fas fa-plus-circle"></i> Agregar  Nuevo</a></button>
+        <button><a href=""> <i class="fas fa-upload"></i> Exportar</a></button>
+      </div>
+       <div class="Container_Search">
+          <input type="text" placeholder="Buscar Menu" style="padding: 10px 20px; border: none; border-radius: 4px; margin: 10px 0px; ">
+          <button style="padding: 10px 20px; border: none; border-radius: 4px; margin: 10px 0px; background-color: #16a085; color: white; "><i class="fas fa-search"></i></button>
+       </div>
+       <div class="Container_Result">
+       <label for="opciones">Resultado</label>
+        <select id="opciones" name="opciones">
+            <option value="opcion1">5</option>
+            <option value="opcion2">10</option>
+            <option value="opcion3">20</option>
+            <option value="opcion3">Todos</option>
+
+        </select>
+       </div>
+
+     </div>
+
     </article>
+   
     
+
 <table class="shadows" style=" width: 100%; background-color: white; max-width: 90%; margin: auto; margin-top: 20px; ">
 <thead class=" " style=" background-color:  #16a085; "  >
 

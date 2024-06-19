340706053021b6edc874dc63129cb0aecd499ee2
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-19 15:27:31 -0500

    Refactor menu layout and improve form styling in AgregarMenu.php

--- src/componets/AgregarMenu.php
+++ src/componets/AgregarMenu.php
@@ -1,27 +1,30 @@
+
 <section style="background-color: whitesmoke; ">
-<div style="display: flex;align-items: center;  margin: .3% 3%; gap:5px;" class="Container_House">
-      <a href="" > <i  style="color: #16a085;" class="fas fa-home"></i></a>
-      <p style="color:#5c5b60;">/ Menu</p>
-     </div>
- <form class="shadows" style="background-color: white; max-width: 60%; margin: auto; display: flex ; flex-direction: column; margin-top: 3%; gap: 12px;" > 
+
+ <form class="shadows" style="background-color: white; max-width: 55%; width: auto; margin: auto; display: flex ; flex-direction: column; margin-top: 3%; gap: 12px;" > 
  <div style="background-color: #16a085;">
         <h1  style=" margin-top: 20px;color: white; padding-left: 20px; ">REGISTRAR NUEVO USUARIO</h1>
 </div>
- <div class="" style="padding: 0px 22px; display:flex; gap: 10px; align-items: center;">
-    <label style="font-size: 21px;" for="exampleInputEmail1" class="form-label">Nombre del Menu:</label>
-    <input style="padding: 6px 3px; font-size: 17px;" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+ <div class="" style="padding: 23px 15px;  display: flex;  flex-wrap: wrap; gap: 25px; text-align: center;">
+    <div class="" style="display: flex; align-items: center;  flex: 1 1 400px; gap: 9px;">
+      <label style="font-size: 18px; color: gray;" for="exampleInputEmail1" class="form-label">Nombre del Menu:</label>
+      <input style="padding: 3px 3px; font-size: 17px;" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div style="display: flex; align-items: center;   flex: 1 1 400px; gap: 9px;">
+      <labe style="font-size: 18px; color: gray;" for="exampleInputPassword1" class="form-label">Precio Menu: </labe>
+      <input style="padding: 3px 3px; font-size: 17px;" type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="" style=" flex: 1 1 400px; display: flex; align-items: center;gap: 9px;">
+    <label style="font-size: 18px; color: gray;" for="exampleInputPassword1" class="form-label">Estado del Menu: </label>
+    <input style="padding: 3px 3px; font-size: 17px;" type="password" class="form-control" id="exampleInputPassword1">
   </div>
-  <div class="">
-    <label for="exampleInputPassword1" class="form-label">Precio Menu</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+
+  
+      
   </div>
-  <div class="">
-    <label for="exampleInputPassword1" class="form-label">Estado del Menu</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div>
-  <button type="submit" class="btn btn-primary">Guardar</button>
-  <button type="submit" class="btn btn-primary">Volver</button>
+  <div class="" style="display: flex; justify-content: center; gap: 9px; margin: 3% 0px;">
+  <button class="Botones" style="background-color: #007cfd; "> <i class="fas fa-save"></i> Guardar</button>
+  <a class="Botones" style="background-color: red; "  href=""> <i class="fas fa-arrow-left"></i> Volver</a>
   </div>
  
 
@@ -31,5 +34,20 @@
 <style>
     .shadows{
     box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+}
+.Botones{
+  
+  cursor: pointer;
+   color: white;
+   border: none;
+   padding: 10px 19px; 
+   border-radius: 4px ;
+  font-weight: 600;
+  text-decoration: none;
+  transition: all 0.1s ease-in-out;
+
+}
+.Botones:hover{
+  transform: scale(1.05);
 }
 </style>

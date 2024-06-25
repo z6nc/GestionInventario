f8f4511fe6c761c4c56f6408071422c164e5dc62
z6nc <andresvelasquezarotinco@gmail.com>
2024-06-25 10:54:39 -0500

    chore: Refactor menu layout and handle existing menu name in AgregarMenuController.php

--- src/js/AlertaRespuestCrudMenu.js
+++ src/js/AlertaRespuestCrudMenu.js
@@ -1,52 +1,50 @@
 window.onload = function () {
-    let TextoRespuesta = document.getElementById('TextoRespuesta');
-    const urlParams = new URLSearchParams(window.location.search);
-    const exitoMenu = urlParams.get('success');
-    const errorMenu = urlParams.get('error');
-    let alerta = document.getElementById('alerta');
-  
-    switch (exitoMenu) {
-      case 'MenuRegisteredSuccessfully':
+  let TextoRespuesta = document.getElementById('TextoRespuesta');
+  const urlParams = new URLSearchParams(window.location.search);
+  const exitoMenu = urlParams.get('success');
+  const errorMenu = urlParams.get('error');
+  let alerta = document.getElementById('alerta');
+
+  switch (exitoMenu) {
+    case 'MenuRegisteredSuccessfully':
+      alerta.style.display = 'flex';
+      alerta.classList.add('StyleSuccess');
+      TextoRespuesta.innerText = 'Menu registrado correctamente';
+      break;
+    case 'MenuDeletedSuccessfully':
+      alerta.style.display = 'flex';
+      alerta.classList.add('StyleSuccess');
+      TextoRespuesta.innerText = 'Menu eliminado correctamente';
+      break;
+    case 'MenuEdited':
+      alerta.style.display = 'flex';
+      alerta.classList.add('StyleSuccess');
+      TextoRespuesta.innerText = 'Menu editado correctamente';
+      break;
+    case 'Menuexist':
+      alerta.style.display = 'flex';
+      alerta.classList.add('StyleError');
+      TextoRespuesta.innerText = 'El nombre del menu ya existe';
+      break;
+    default:
+      if (errorMenu === 'MenuNotRegisteredSuccessfull') {
         alerta.style.display = 'flex';
-        alerta.classList.add('StyleSuceess');
-        TextoRespuesta.innerText = 'Menu registrado Correctamente';
-        break;
-      case 'MenuDeletedSuccessfully':
+        alerta.classList.add('StyleError');
+        TextoRespuesta.innerText = 'Menu no se pudo editar';
+      } else if (errorMenu === 'MenuNotDeletedSuccessfull') {
         alerta.style.display = 'flex';
-        alerta.classList.add('StyleSuceess');
-        TextoRespuesta.innerText = 'Menu eliminado Correctamente';
-        break;
-      case 'MenuEdited':
+        alerta.classList.add('StyleError');
+        TextoRespuesta.innerText = 'Menu no se pudo eliminar';
+      } else if (errorMenu === 'MenuNotMenuEdited') {
         alerta.style.display = 'flex';
-        alerta.classList.add('StyleSuceess');
-        TextoRespuesta.innerText = 'Menu editado Correctamente';
-        break;
-        case 'Menuexist':
-          alerta.style.display = 'flex';
-          alerta.classList.add('StyleError');
-          TextoRespuesta.innerText = 'El nombre del menu ya existe';
-          break;
-      default:
-        if (errorMenu === 'MenuNotRegisteredSuccessfull') {
-          alerta.style.display = 'flex';
-          alerta.classList.add('StyleError');
-          TextoRespuesta.innerText = 'Menu no registrado Correctamente';
-        } else if (errorMenu === 'MenuNotDeletedSuccessfull') {
-          alerta.style.display = 'flex';
-          alerta.classList.add('StyleError');
-          TextoRespuesta.innerText = 'Menu no se pudo eliminar';
-          
-        } else if (errorMenu === 'MenuNotMenuEdited') {
-          alerta.style.display = 'flex';
-          alerta.classList.add('StyleError');
-          TextoRespuesta.innerText = 'Menu no se pudo Editar';
-        } 
-        else {
-          alerta.style.display = 'none';
-        }
-    }
-  
-    document.querySelector('#alerta span').addEventListener('click', () => {
-      alerta.style.display = 'none';
-    });
-  };
+        alerta.classList.add('StyleError');
+        TextoRespuesta.innerText = 'Menu no se pudo editar';
+      } else {
+        alerta.style.display = 'none';
+      }
+  }
+
+  document.querySelector('#alerta span').addEventListener('click', () => {
+    alerta.style.display = 'none';
+  });
+};

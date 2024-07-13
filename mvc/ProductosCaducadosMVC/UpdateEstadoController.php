2b2f844e9d9a45c2463c097978c6a4bf91481dbe
z6nc <andresvelasquezarotinco@gmail.com>
2024-07-13 11:42:13 -0500

    Refactor UpdateEstadoController.php to fix variable name typo and add success and error redirects

--- mvc/ProductosCaducadosMVC/UpdateEstadoController.php
+++ mvc/ProductosCaducadosMVC/UpdateEstadoController.php
@@ -6,6 +6,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $idTransaccion = $_POST['ID_TRANSACCION'];
 }
 $UpdateEstado = new ProductoCaducidoMode($conn);
-$UpdateEstado->UpdateEstado($idTransaccion);
+$UpdateEstados->UpdateEstado($idTransaccion);
+if ($UpdateEstados ) {
 header('Location: ../../src/inicio.php?success=UpdateEstado');
+} else{
+header('Location: ../../src/inicio.php?error=UpdateEstado');
+}
 ?>
